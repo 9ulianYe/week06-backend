08eb877e0b43cf6ca9e413f771bb03470e5071ca
JulianYe <9ulianye@gmail.com>
2025-07-10 14:41:05 +0800

    Q3.輸入一段字，輸出每個之間多一個-

--- week06-backend/week06-backend/Program.cs
+++ week06-backend/week06-backend/Program.cs
@@ -3,26 +3,45 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {   //CH6. 字串
             //Q1.輸入姓名，輸出 Hi~輸入的姓名，比如說輸入Justin，輸出Hi~Justin。
+
             //Console.Write("輸入姓名 : ");
             //string input = Console.ReadLine();
             //Console.WriteLine($"Hi~{input}");
 
             //Q2. 請輸入任何一個字，是否出現在”人人為我，我為人人、饒人不癡漢，癡漢不饒人”這個字串裡。
-            Console.Write("請輸入任何一個字 : ");
-            string input = Console.ReadLine();
-            string sentcne = "人人為我，我為人人、饒人不癡漢，癡漢不饒人";
-            if (sentcne.Contains(input)) 
-            {
-                Console.WriteLine($"\"{input}\"在句子中");
-            }
-            else
-            {
-                Console.WriteLine($"\"{input}\"不在句子中");
-            }
 
-        }
+            //Console.Write("請輸入任何一個字 : ");
+            //string input = Console.ReadLine();
+            //string sentcne = "人人為我，我為人人、饒人不癡漢，癡漢不饒人";
+            //if (sentcne.Contains(input)) 
+            //{
+            //    Console.WriteLine($"\"{input}\"在句子中");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"\"{input}\"不在句子中");
+            //}
+
+            //Q3.輸入一段字，輸出每個之間多一個-，如輸入apple ，輸出a-p-p-l-e。
+
+            Console.Write("請輸入一個字 : ");
+            string input = Console.ReadLine();
+            char[] chars = input.ToCharArray();
+            string newString = " ";
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                newString += chars[i];
+                if (i < chars.Length-1)
+                {
+                    newString += "-";
+                }
+            }
+            Console.WriteLine(newString);
+
+        }   
 
     }
 }

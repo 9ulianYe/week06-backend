94063648a5bfc877862ea97410c010c3e15d68f6
JulianYe <9ulianye@gmail.com>
2025-07-10 14:11:36 +0800

    Q1.輸入姓名，輸出 Hi~輸入的姓名，比如說輸入Justin，輸出Hi~Justin。

--- week06-backend/week06-backend/Program.cs
+++ week06-backend/week06-backend/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            
+            //Q1.輸入姓名，輸出 Hi~輸入的姓名，比如說輸入Justin，輸出Hi~Justin。
+            Console.Write("輸入姓名 : ");
+            string input = Console.ReadLine();
+            Console.WriteLine($"Hi~{input}");
         }
     }
 }

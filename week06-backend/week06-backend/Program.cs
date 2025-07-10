9aefdfc86a85010db47fec852283fd1913dd8be6
JulianYe <9ulianye@gmail.com>
2025-07-10 14:28:02 +0800

    Q2. 請輸入任何一個字，是否出現在”人人為我，我為人人、饒人不癡漢，癡漢不饒人”這個字串裡。

--- week06-backend/week06-backend/Program.cs
+++ week06-backend/week06-backend/Program.cs
@@ -5,9 +5,24 @@
         static void Main(string[] args)
         {
             //Q1.輸入姓名，輸出 Hi~輸入的姓名，比如說輸入Justin，輸出Hi~Justin。
-            Console.Write("輸入姓名 : ");
+            //Console.Write("輸入姓名 : ");
+            //string input = Console.ReadLine();
+            //Console.WriteLine($"Hi~{input}");
+
+            //Q2. 請輸入任何一個字，是否出現在”人人為我，我為人人、饒人不癡漢，癡漢不饒人”這個字串裡。
+            Console.Write("請輸入任何一個字 : ");
             string input = Console.ReadLine();
-            Console.WriteLine($"Hi~{input}");
+            string sentcne = "人人為我，我為人人、饒人不癡漢，癡漢不饒人";
+            if (sentcne.Contains(input)) 
+            {
+                Console.WriteLine($"\"{input}\"在句子中");
+            }
+            else
+            {
+                Console.WriteLine($"\"{input}\"不在句子中");
+            }
+
         }
+
     }
 }

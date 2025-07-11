f3cbca5b579799d7dcb6b3dc5d64168e8a7522eb
JulianYe <9ulianye@gmail.com>
2025-07-11 13:35:36 +0800

    Q5.輸入一個大於五個字的單字，若小於五個字輸出長度不夠，若大於五個字，則輸出前三個字

--- week06-backend/week06-backend/Program.cs
+++ week06-backend/week06-backend/Program.cs
@@ -45,20 +45,33 @@ namespace week06_backend
             //Console.WriteLine(newString);
 
             //Q4. 輸入一個檔名輸出副檔名，如輸入apple.jpg，輸出jpg
-            Console.Write("請輸入檔名： ");
-            string fileName = Console.ReadLine();
-            string pattern =@"^[^\\/:*?<>|]+\.[a-zA-Z0-9]+$";
-            if (Regex.IsMatch(fileName, pattern)) 
-            {
-                string[] fileNamePart = fileName.Split('.');
-                string fileExtention = fileNamePart[^1];
-                Console.WriteLine($"副檔名是：{fileExtention}" );
-            }
-            else
-            {
-                Console.WriteLine("檔名格式不正確！");
-            }
 
+            //Console.Write("請輸入檔名： ");
+            //string fileName = Console.ReadLine();
+            //string pattern =@"^[^\\/:*?<>|]+\.[a-zA-Z0-9]+$";
+            //if (Regex.IsMatch(fileName, pattern)) 
+            //{
+            //    string[] fileNamePart = fileName.Split('.');
+            //    string fileExtention = fileNamePart[^1];
+            //    Console.WriteLine($"副檔名是：{fileExtention}" );
+            //}
+            //else
+            //{
+            //    Console.WriteLine("檔名格式不正確！");
+            //}
+
+            //Q5.輸入一個大於五個字的單字，若小於五個字輸出長度不夠，若大於五個字，則輸出前三個字
+            
+            Console.Write("請輸入一個單字：");
+            string word = Console.ReadLine();
+            if (word.Length < 5)
+            {
+                Console.WriteLine("長度不夠");
+            }
+            else {
+                string firstThreeword = word.Substring(0, 3);
+                Console.WriteLine($"前三個字是：{firstThreeword}");
+            }
         }   
 
     }
